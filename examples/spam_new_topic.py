8561497b3f4794cd3ef82a4b4762689525a9f840
un1xcorn <55710811+un1xcorn@users.noreply.github.com>
2022-01-22 11:26:21 +0000

    Create spam_new_topic.py

---
+++ examples/spam_new_topic.py
@@ -0,0 +1,16 @@
+from os import system, name
+import sys, time
+
+sys.path.append("./../")
+from jvc_api import JVC
+
+#A simple python script which send a message on a random topic from the first page
+
+client = JVC("", JVC.FORUM_1825)
+
+while True:
+    time.sleep(1)
+    title = "My topic is impressive."
+    message = "Beautiful evening..."
+    if client.createTopic(title, message):
+        print("Success!")
